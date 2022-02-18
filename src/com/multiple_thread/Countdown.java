6aecfb607bb882465bcd32e79ffc216998ea1fbb
Ary Kurniadi Irawan <arykurniadiirawan@yahoo.co.id>
2022-02-18 14:53:17 +0100

    Multiple Thread added

---
+++ src/com/multiple_thread/Countdown.java
@@ -0,0 +1,24 @@
+package com.multiple_thread;
+
+public class Countdown {
+
+    public void doCountDown(){
+        String color;
+
+        switch (Thread.currentThread().getName()){
+            case "Thread 1":
+                color = ThreadColor.ANSI_CYAN;
+                break;
+            case "Thread 2":
+                color = ThreadColor.ANSI_PURPLE;
+                break;
+            default:
+                color = ThreadColor.ANSI_GREEN;
+                break;
+        }
+
+        for(int i=10; i>0;i--){
+            System.out.println(color+Thread.currentThread().getName()+": i ="+i);
+        }
+    }
+}
