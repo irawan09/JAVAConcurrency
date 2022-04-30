1da5cb60356f2ce555ac5c1275b2044c64d0e2fd
Ary Kurniadi Irawan <arykurniadiirawan@yahoo.co.id>
2022-04-30 10:54:59 +0200

    Update

---
+++ src/another_sources/BasicMultithreading/Lecture9/Lecture9.java
@@ -0,0 +1,34 @@
+package another_sources.BasicMultithreading.Lecture9;
+
+public class Lecture9 {
+
+    public static void main(String[] args){
+
+        Processor processor = new Processor();
+
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    processor.producer();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    processor.consumer();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+    }
+}
