c14ee44add071df05e38476214c0a42a2ca03573
Ary Kurniadi Irawan <arykurniadiirawan@yahoo.co.id>
2022-04-30 15:37:53 +0200

    Update

---
+++ src/AnotherSources/BasicMultithreading/Lecture12/Counter.java
@@ -0,0 +1,21 @@
+package AnotherSources.BasicMultithreading.Lecture12;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Counter extends Thread {
+
+    AtomicInteger count;
+
+    Counter(){
+        count = new AtomicInteger();
+    }
+
+    public void run(){
+
+        int max = 1_000_000;
+
+        for (int i=0; i<max;i++){
+            count.addAndGet(1);
+        }
+    }
+}
---
+++ src/AnotherSources/BasicMultithreading/Lecture12/Lecture12.java
@@ -0,0 +1,27 @@
+package AnotherSources.BasicMultithreading.Lecture12;
+
+import static AnotherSources.BasicMultithreading.Lecture12.ThreadColor.*;
+
+public class Lecture12 {
+
+    public static void main(String[] args){
+
+        Counter counter = new Counter();
+
+        Thread t1 = new Thread(counter, ANSI_BLUE+"First Thread");
+        Thread t2 = new Thread(counter, ANSI_RED+"Second Thread");
+
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(ANSI_PURPLE+"Counter: "+counter.count);
+    }
+}
+
