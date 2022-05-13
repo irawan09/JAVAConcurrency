30be08e7d5fcf5c5512da135629a1701a2afda83
Ary Kurniadi Irawan <arykurniadiirawan@yahoo.co.id>
2022-05-13 11:11:15 +0200

    Update

--- src/AnotherSources/ConcurrentLibrary/BlockingQueue/App.java
+++ src/AnotherSources/ConcurrentLibrary/BlockingQueue/App.java
@@ -5,6 +5,20 @@ import java.util.concurrent.BlockingQueue;
 
 public class App {
 
+    /**
+     *
+     * 	BlockingQueue -> an interface that represents a queue that is thread safe
+     * 		Put items or take items from it ...
+     *
+     * 		For example: one thread putting items into the queue and another thread taking items from it
+     * 			at the same time !!!
+     * 				We can do it with producer-consumer pattern !!!
+     *
+     * 		put() putting items to the queue
+     * 		take() taking items from the queue
+     *
+     */
+
     public static void main(String[] args){
 
         BlockingQueue<String> queue = new ArrayBlockingQueue<>(10);
