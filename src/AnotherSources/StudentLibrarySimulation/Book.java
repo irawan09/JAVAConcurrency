d427079fc553fc51b9fbb336a3dd8a187fb14851
Ary Kurniadi Irawan <arykurniadiirawan@yahoo.co.id>
2022-05-15 15:31:51 +0200

    Update

---
+++ src/AnotherSources/StudentLibrarySimulation/Book.java
@@ -0,0 +1,30 @@
+package AnotherSources.StudentLibrarySimulation;
+
+import static AnotherSources.StudentLibrarySimulation.ThreadColor.*;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Book {
+
+    private int id;
+    private Lock lock;
+
+    public Book(int id) {
+        this.lock = new ReentrantLock();
+        this.id = id;
+    }
+
+    public void read(Student student) throws InterruptedException{
+        lock.tryLock(10, TimeUnit.MILLISECONDS);
+        System.out.println(ANSI_BLUE+student+" starts reading"+this);
+        Thread.sleep(2000);
+        lock.unlock();
+        System.out.println(ANSI_RED+student+" has finished reading"+this);
+    }
+
+    @Override
+    public String toString() {
+        return "Book"+id;
+    }
+}
