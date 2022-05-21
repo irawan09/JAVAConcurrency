c4324545a68572f2ca1615d49efaac9d18928686
Ary Kurniadi Irawan <arykurniadiirawan@yahoo.co.id>
2022-05-21 12:19:15 +0200

    Update

---
+++ src/AnotherSources/ForkJoinFramework/ForkJoinSimple1/SimpleRecursiveAction.java
@@ -0,0 +1,22 @@
+package AnotherSources.ForkJoinFramework.ForkJoinSimple1;
+
+import static AnotherSources.ForkJoinFramework.ForkJoinSimple1.ThreadColor.*;
+import java.util.concurrent.RecursiveAction;
+
+public class SimpleRecursiveAction extends RecursiveAction {
+
+    private int simulateWork;
+
+    public SimpleRecursiveAction(int simulateWork) {
+        this.simulateWork = simulateWork;
+    }
+
+    @Override
+    protected void compute() {
+        if(simulateWork>100){
+            System.out.println(ANSI_RED+"Parallel execution and split the tasks... "+simulateWork);
+        } else{
+            System.out.println(ANSI_BLUE+"No need for parallel execution, sequential is OK for computation... "+simulateWork);
+        }
+    }
+}
